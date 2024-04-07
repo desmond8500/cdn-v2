b92f92ae5e5c34568fb00be9106841b34152919d
kratos <desmond8500@gmail.com>
2024-04-07 00:47:00 +0000

    update

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const path = require('path');
 const app = express()
 const port = 3000
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
