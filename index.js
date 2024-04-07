e24362e3cf1d6442b4652940aa067b5e140be13d
kratos <desmond8500@gmail.com>
2024-04-07 09:23:31 +0000

    update

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 const express = require('express')
-const path = require('path');
 const app = express()
-const port = 3000
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.get('/css', (req, res) => {
-  res.send('Css')
-})
-app.get('/html', (req, res) => {
-  res.send('html')
-})
 
 app.use(express.static('public'))
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(process.env.PORT, function () {
+  console.log(`Example app listening on port`)
 })
