7321bb779cfbbbadd65b2390069a87b5618de285
kratos <desmond8500@gmail.com>
2024-04-08 07:28:36 +0000

    update

--- local.js
+++ local.js
@@ -7,15 +7,17 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.get('/css', (req, res) => {
-  res.send('Css')
-})
-app.get('/html', (req, res) => {
-  res.send('html')
-})
 
 app.use(express.static('public'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+// r
+app.get('/css', (req, res) => {
+  res.send('Css')
+})
+app.get('/html', (req, res) => {
+  res.send('html')
 })
