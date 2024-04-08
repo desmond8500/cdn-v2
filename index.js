ea658448a0ee5ba561cf94f42175104854d54499
kratos <desmond8500@gmail.com>
2024-04-08 08:02:16 +0000

    update

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ app.use(express.static('public'))
 app.listen(process.env.PORT, function () {
   console.log(`Example app listening on port`)
 })
+
+app.get('/tabler', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/tabler/tabler.html'));
+})
