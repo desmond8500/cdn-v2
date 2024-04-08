532743bf4d8c2b6eed00d3e2700b3b5677c5383c
kratos <desmond8500@gmail.com>
2024-04-08 08:02:55 +0000

    fix

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+app.get('/tabler', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/tabler/tabler.html'));
+})
 
 app.use(express.static('public'))
 
 app.listen(process.env.PORT, function () {
   console.log(`Example app listening on port`)
-})
-
-app.get('/tabler', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/tabler/tabler.html'));
 })
